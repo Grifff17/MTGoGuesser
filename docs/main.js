0408e8145e40fc0e1b91f50aca83cec0f864f0e8
Griffin Riley <rileyg@oregonstate.edu>
2024-12-18 22:58:32 -0800

    added artist, changed query to all lands after 2004

--- docs/main.js
+++ docs/main.js
@@ -7,13 +7,16 @@ function loadPage(event) {
 }
 
 function loadImage(event) {
-    fetch('https://api.scryfall.com/cards/random?q=t%3A"basic+land"+unique%3Aart+atag:plane')
-        .then(response => { return response.json() }
-        ).then(data => {
+    fetch('https://api.scryfall.com/cards/random?q=t%3A"land"+unique%3Aart+atag:plane+year%3E%3D2004')
+        .then(response => { return response.json() })
+        .then(data => {
             card = data
 
             let name = document.getElementById("card_set")
             name.innerText = ""
+
+            let artist = document.getElementById("card_artist")
+            artist.innerText = card.artist
 
             let img = document.getElementById("card_img")
             img.src = card.image_uris.art_crop
